de0467d6b3737ed826c857399277585f00ea2f33
bamboo_octopus <bamboooctopus@gmail.com>
2023-01-03 21:49:23 -0500

    number buttons and clear button work

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let buttons = document.getElementById("buttons").childNodes;
 
 let screen = document.getElementById("big-text");
 
+const numArray = [];
+
 buttons.forEach(button => button.addEventListener("click", logButton));
 
 function logButton() {
@@ -9,6 +11,20 @@ function logButton() {
 
     if (this.id.split("")[0] == "n") {
         splitArray = this.id.split("");
-        screen.textContent = splitArray[splitArray.length - 1];
+        let num = screen.textContent = splitArray[splitArray.length - 1];
+        numArray.push(num);
+        screen.textContent = numArray.join("");
+        
+    }
+    else if (this.id == "clear") {
+        let length = numArray.length;
+        console.log(length);
+        
+        for (let i = 0; i < length; i ++) {
+            numArray.pop();
+            screen.textContent = numArray;
+        }
+
+        
     }
 }
